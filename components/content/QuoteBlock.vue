7d68ffbf8b05d1f9124a2f104e20d4e9e6e14a6f
Wouter Kettlitz <w.kettlitz@edumundo.com>
2025-02-01 16:19:35 +0100

    add quote blocks

---
+++ components/content/QuoteBlock.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ citationUrl?: string }>();
+</script>
+
+<template>
+  <blockquote :cite="citationUrl" class="p-3 bg-gray-200 dark:bg-gray-500">
+    <slot mdc-unwrap="p" />
+  </blockquote>
+</template>
